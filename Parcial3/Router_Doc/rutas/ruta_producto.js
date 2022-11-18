af74d170f73b6c8f16d33d86174984dde00030b7
GemaMeraz <gema_nataly05@hotmail.com>
2022-11-18 08:09:49 -0600

    Router documentado

---
+++ Parcial3/Router_Doc/rutas/ruta_producto.js
@@ -0,0 +1,126 @@
+let mysql = require('mysql')
+const express = require('express')
+var router = express.Router()
+// const {dirname} = require('path')
+
+
+
+var con = mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'',
+    database:'gm19100218'
+});
+
+con.connect((err)=>{
+    if(err){
+        throw err;
+    }
+    console.log('conectado')
+})
+
+
+/**
+ * @swagger
+ * /producto:
+ *  get:
+ *     description: Welcome to swagger-jsdoc
+ *     responses:
+ *        200: 
+ *          description: Returns a mysterious string
+ */
+
+//Consultar todos los productos
+router.get('/' ,(req,res)=>{
+   
+    con.query('SELECT * FROM producto', function(error,results, fields){
+        res.send(results)
+        if(error) throw error;
+    });
+    
+})
+
+// consultar un producto en particular
+.get('/:idProducto', (req,res)=>{
+    const id=req.params.idProducto;
+    con.query(`SELECT * FROM producto WHERE idProducto=${id}`, function(error,results, fields){
+        res.send(results)
+        if(error) throw error;
+    });
+    con.end();
+})
+
+
+/**
+ * @swagger
+ * /producto:
+ *  post:
+ *     description: Welcome to swagger-jsdoc
+ *     responses:
+ *        200: 
+ *          description: Returns a mysterious string
+ */
+
+// hacer el alta del producto
+.post('/',(req,res)=>{
+    con.query(`INSERT INTO producto (nomProducto, marcaProd,catProd,contProd,proveedor,desProd,precioProd) VALUES("${req.body.nomProducto}", "${req.body.marcaProd}", "${req.body.catProd}", "${req.body.contProd}", "${req.body.proveedor}", "${req.body.desProd}", ${req.body.precioProd})`, function(error,results, fields){
+        res.send('Ingreso de datos realizado')
+        if(error) throw error;
+    });
+})
+
+/**
+ * @swagger
+ * /producto:
+ *  delete:
+ *     description: Welcome to swagger-jsdoc
+ *     responses:
+ *        200: 
+ *          description: Returns a mysterious string
+ */
+
+
+//Eliminar el producto
+.delete('/:idProducto',(req,res)=>{
+    const id=req.params.idProducto;
+    con.query(`DELETE FROM producto WHERE idProducto=${id}`, function(error,results, fields){
+        res.json('Producto eliminado correctamente')
+        if(error) throw error;
+    });
+    con.end()
+})
+
+/**
+ * @swagger
+ * /producto:
+ *  put:
+ *     description: Welcome to swagger-jsdoc
+ *     responses:
+ *        200: 
+ *          description: Returns a mysterious string
+ */
+
+
+//modificar un producto
+.put('/:idProducto',(req,res)=>{
+    console.log(req.query)
+    const id=req.params.idProducto;
+    let params={
+        'nomProducto':req.query.nomProducto,
+        'marcaProd':req.query.marcaProd,
+        'catProd': req.query.catProd,
+        'contProd': req.query.contProd,
+        'proveedor': req.query.proveedor,
+        'desProd': req.query.desProd, 
+        'precioProd': req.query.precioProd
+    }
+    
+    let mysql =`UPDATE producto SET ? WHERE idProducto=${id}`
+    let bod = con.query(mysql,params,(err,result)=>{
+        if(err) throw err;
+        res.send('modificacion hecha')
+    });
+});
+
+
+module.exports.router=router;
